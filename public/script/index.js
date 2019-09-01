a9c65bd17d470cd52fda0f64f57e5b2ba3d5812e
Leaf <Leaf>
2019-09-01 00:20:18 -0700

    add input listeners

--- public/script/index.js
+++ public/script/index.js
@@ -10,19 +10,97 @@ const TANK_INNER_HEIGHT = TANK_HEIGHT - 2 * TANK_MARGIN;
 const TANK_CANNON_LENGTH = TANK_WIDTH;
 const TANK_CANNON_THICC = 6;
 const TANK_CANNON_THICC_HALF = 3;
+const KEY_UP = "ArrowUp";
+const KEY_DOWN = "ArrowDown";
+const KEY_LEFT = "ArrowLeft";
+const KEY_RIGHT = "ArrowRight";
+class Game {
+    constructor() {
+    }
+}
+window.G = new Game();
+class Input {
+    constructor() {
+        this.left = false;
+        this.right = false;
+        this.up = false;
+        this.down = false;
+        this.fire = false;
+        this.mine = false;
+        this.x = 0;
+        this.y = 0;
+    }
+}
+window.I = new Input();
+let tanksLayer = document.createElement('canvas');
+tanksLayer.width = CANVAS_WIDTH;
+tanksLayer.height = CANVAS_HEIGHT;
+let tanksCX = tanksLayer.getContext("2d");
+let C;
 $(document).ready(function () {
-    let C = document.getElementById("canvas");
+    C = document.getElementById("canvas");
     C.width = CANVAS_WIDTH;
     C.height = CANVAS_HEIGHT;
     let X = C.getContext("2d");
-    let tanksLayer = document.createElement('canvas');
-    tanksLayer.width = CANVAS_WIDTH;
-    tanksLayer.height = CANVAS_HEIGHT;
-    let tanksCX = tanksLayer.getContext("2d");
     drawTankBase(tanksCX, 40, 70, "rgb(255,30,30)");
     drawTankCannon(tanksCX, 40, 70, Math.PI / 2);
     X.drawImage(tanksLayer, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    bindInputEvents(C);
 });
+function bindInputEvents(e) {
+    e.addEventListener("keydown", function (ev) {
+        switch (ev.key) {
+            case KEY_UP:
+                window.I.up = true;
+                break;
+            case KEY_DOWN:
+                window.I.down = true;
+                break;
+            case KEY_LEFT:
+                window.I.left = true;
+                break;
+            case KEY_RIGHT:
+                window.I.right = true;
+                break;
+        }
+    });
+    window.addEventListener("keyup", function (ev) {
+        switch (ev.key) {
+            case KEY_UP:
+                window.I.up = false;
+                break;
+            case KEY_DOWN:
+                window.I.down = false;
+                break;
+            case KEY_LEFT:
+                window.I.left = false;
+                break;
+            case KEY_RIGHT:
+                window.I.right = false;
+                break;
+        }
+    });
+    e.addEventListener("mousemove", function (ev) {
+        ev.preventDefault();
+        let rect = C.getBoundingClientRect();
+        window.I.x = (ev.clientX - rect.left) * C.width / rect.width;
+        window.I.y = (ev.clientY - rect.top) * C.height / rect.height;
+        $("#txt").text("" + window.I.x + "," + window.I.y);
+    });
+    e.addEventListener("click", function (ev) {
+        ev.preventDefault();
+        let rect = C.getBoundingClientRect();
+        window.I.x = (ev.clientX - rect.left) * C.width / rect.width;
+        window.I.y = (ev.clientY - rect.top) * C.height / rect.height;
+        $("#txt").text("click " + window.I.x + "," + window.I.y);
+    });
+    e.addEventListener("contextmenu", function (ev) {
+        ev.preventDefault();
+    });
+}
+function moveTank() {
+    //
+}
 function drawTankBase(X, x, y, color) {
     x -= TANK_WIDTH_HALF;
     y -= TANK_HEIGHT_HALF;
--- public/script/index.ts
+++ public/script/index.ts
@@ -10,21 +10,120 @@ const TANK_INNER_HEIGHT = TANK_HEIGHT-2*TANK_MARGIN;
 const TANK_CANNON_LENGTH = TANK_WIDTH;
 const TANK_CANNON_THICC = 6;
 const TANK_CANNON_THICC_HALF = 3;
+
+const KEY_UP = "ArrowUp";
+const KEY_DOWN = "ArrowDown";
+const KEY_LEFT = "ArrowLeft";
+const KEY_RIGHT = "ArrowRight";
+
+interface Window {
+	G:Game;
+	I:Input;
+}
+
+class Game{
+	constructor(){
+	}
+}
+window.G = new Game();
+class Input{
+	left:boolean;
+	right:boolean;
+	up:boolean;
+	down:boolean;
+	fire:boolean;
+	mine:boolean;
+	x:number;
+	y:number;
+	constructor(){
+		this.left = false;
+		this.right = false;
+		this.up = false;
+		this.down = false;
+		this.fire = false;
+		this.mine = false;
+		this.x = 0;
+		this.y = 0;
+	}
+}
+window.I = new Input();
+
+let tanksLayer = document.createElement('canvas');
+tanksLayer.width = CANVAS_WIDTH;
+tanksLayer.height = CANVAS_HEIGHT;
+let tanksCX = tanksLayer.getContext("2d") as CanvasRenderingContext2D;
+
+let C:HTMLCanvasElement;
+
 $(document).ready(function() {
-	let C = document.getElementById("canvas") as HTMLCanvasElement;
+	C = document.getElementById("canvas") as HTMLCanvasElement;
 	C.width = CANVAS_WIDTH;
 	C.height = CANVAS_HEIGHT;
 	let X = C.getContext("2d") as CanvasRenderingContext2D;
 
-	let tanksLayer = document.createElement('canvas');
-	tanksLayer.width = CANVAS_WIDTH;
-	tanksLayer.height = CANVAS_HEIGHT;
-	let tanksCX = tanksLayer.getContext("2d");
-
 	drawTankBase(tanksCX,40,70,"rgb(255,30,30)");
 	drawTankCannon(tanksCX, 40,70,Math.PI / 2);
 	X.drawImage(tanksLayer,0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+
+	bindInputEvents(C);
 });
+
+function bindInputEvents(e:HTMLElement) {
+	e.addEventListener("keydown",function(ev:KeyboardEvent) {
+		switch(ev.key) {
+			case KEY_UP:
+				window.I.up = true;
+				break;
+			case KEY_DOWN:
+				window.I.down = true;
+				break;
+			case KEY_LEFT:
+				window.I.left = true;
+				break;
+			case KEY_RIGHT:
+				window.I.right = true;
+				break;
+		}
+	});
+	window.addEventListener("keyup",function(ev:KeyboardEvent) {
+		switch(ev.key) {
+			case KEY_UP:
+				window.I.up = false;
+				break;
+			case KEY_DOWN:
+				window.I.down = false;
+				break;
+			case KEY_LEFT:
+				window.I.left = false;
+				break;
+			case KEY_RIGHT:
+				window.I.right = false;
+				break;
+		}
+	});
+	e.addEventListener("mousemove",function(ev:MouseEvent) {
+		ev.preventDefault();
+		let rect = C.getBoundingClientRect();
+		window.I.x = (ev.clientX - rect.left) * C.width / rect.width;
+		window.I.y = (ev.clientY - rect.top) * C.height / rect.height;
+		$("#txt").text(""+window.I.x+","+window.I.y);
+	});
+	e.addEventListener("click",function(ev:MouseEvent) {
+		ev.preventDefault();
+		let rect = C.getBoundingClientRect();
+		window.I.x = (ev.clientX - rect.left) * C.width / rect.width;
+		window.I.y = (ev.clientY - rect.top) * C.height / rect.height;
+
+		$("#txt").text("click "+window.I.x+","+window.I.y);
+	});
+	e.addEventListener("contextmenu",function(ev:MouseEvent) {
+		ev.preventDefault();
+	});
+}
+
+function moveTank() {
+	//
+}
 
 function drawTankBase(X: CanvasRenderingContext2D, x, y, color) {
 	x -= TANK_WIDTH_HALF;
