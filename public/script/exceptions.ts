025f1299aa1f945c08685c2383980d0826d0007d
Leaf <Leaf>
2020-06-06 20:31:39 -0700

    indentation

--- public/script/exceptions.ts
+++ public/script/exceptions.ts
@@ -2,12 +2,12 @@ import {Tank} from "./tank.js";
 
 export class GameEvent{}
 export class TankKilledEvent extends GameEvent{
-    killed:Tank;
-    killer:Tank;
-    constructor(killed:Tank, killer:Tank) {
-        super();
-        this.killed = killed;
-        this.killer = killer;
-    }
+	killed:Tank;
+	killer:Tank;
+	constructor(killed:Tank, killer:Tank) {
+		super();
+		this.killed = killed;
+		this.killer = killer;
+	}
 }
 export class ShotExpiredEvent extends GameEvent{}
